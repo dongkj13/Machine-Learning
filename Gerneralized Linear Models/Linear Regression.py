f0a46a98ad7b2019bee81f7cd5d037ecafa75ec4
Dong Kai Jie <504814319@qq.com>
2018-03-01 16:49:37 +0800

    Linear Regression

---
+++ Gerneralized Linear Models/Linear Regression.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+This example uses the only the first feature of the diabetes dataset, 
+in order to illustrate a two-dimensional plot of this regression technique. 
+The straight line can be seen in the plot, showing how linear regression 
+attempts to draw a straight line that will best minimize 
+the residual sum of squares between the observed responses in the dataset, 
+and the responses predicted by the linear approximation.
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Load the diabetes dataset
+diabetes = datasets.load_diabetes()
+
+
+# Use only one feature
+# np.newaxis：令多维数增加一个轴
+# (442, ) -> (442, 1)
+diabetes_X = diabetes.data[:, np.newaxis, 2]
+
+# Split the data into training/testing sets
+diabetes_X_train = diabetes_X[:-20]
+diabetes_X_test = diabetes_X[-20:]
+
+# Split the targets into training/testing sets
+diabetes_y_train = diabetes.target[:-20]
+diabetes_y_test = diabetes.target[-20:]
+
+# Create linear regression object
+regr = linear_model.LinearRegression()
+regr = linear_model.Ridge(alpha = 10)
+regr = linear_model.Lasso(alpha = 1)
+regr = linear_model.RidgeCV(alphas=[0.1, 1.0, 10.0], store_cv_values = True)
+
+# Train the model using the training sets
+regr.fit(diabetes_X_train, diabetes_y_train)
+
+# Make predictions using the testing set
+diabetes_y_pred = regr.predict(diabetes_X_test)
+
+# The coefficients
+print('Coefficients: \n', regr.coef_)
+# The intercept
+print('Intercept: \n', regr.intercept_)
+# The mean squared error
+print("Mean squared error: %.2f"
+      % mean_squared_error(diabetes_y_test, diabetes_y_pred))
+# Explained variance score: 1 is perfect prediction
+print('Variance score: %.2f' % r2_score(diabetes_y_test, diabetes_y_pred))
+# Using regr.score()
+print('Variance score: %.2f' % regr.score(diabetes_X_test, diabetes_y_test))
+
+# Plot outputs
+plt.scatter(diabetes_X_test, diabetes_y_test,  color='black')
+plt.plot(diabetes_X_test, diabetes_y_pred, color='blue', linewidth=3)
+
+plt.xticks(())
+plt.yticks(())
+
+plt.show()
